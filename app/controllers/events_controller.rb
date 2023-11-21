ab75e2c7b10aa4368c3d2760fc26bd0d2a762932
Alejandro Sanz <alex_sanz5@hotmail.com>
2023-11-21 14:16:52 +0000

    Change resources, remove 'authenticate_user', comment out the skip_before_action and started styling the home page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
 
   def home
+  end
+
+  def index
   end
 end
