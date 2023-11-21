760e7d03a4ccf95ca2ce860d9a8f55c7a69af157
Daniel Shaw <danielshaw71@gmail.com>
2023-11-21 13:13:49 +0000

    working on tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,17 @@
 class TicketsController < ApplicationController
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    if @ticket.save
+      redirect_to @ticket, notice: "ticket created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:user_id, :event_id, :start_time, :end_time, :guests)
+  end
 end
