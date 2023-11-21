70a3cf12072d7e9587bc144c9bdd3733c0dd9960
Mark O'Connor <markoconnor1000@gmail.com>
2023-11-21 13:11:09 +0000

    basic seed, events and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,95 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users..."
+
+# categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  user.save!
+  price_array = []
+  x = 30
+  15.times do
+    price_array << x
+    x += 5
+  end
+  capacity_array = []
+    y = 10
+  20.times do
+    capacity_array << y
+    y += 5
+  end
+  5.times do
+    event = Event.new(
+      venue: Faker::Restaurant.name,
+      menu: Faker::Lorem.paragraph(sentence_count: 5),
+      cuisine: Faker::Restaurant.type,
+      price: price_array.sample,
+      description: Faker::Restaurant.description,
+      capacity: capacity_array.sample,
+      date: Faker::Date.forward(from: ::Date.today, days: 365)
+    )
+    event.user = user
+    event.save!
+  end
+end
+
+20.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+# user_id_array =[]
+# n = 1
+# 19.times do
+#   user_id_array << n
+#   n += 1
+# end
+
+# puts user_id_array
+
+# price_array = []
+# x = 30
+# 15.times do
+#   price_array << x
+#   x += 5
+# end
+
+# capacity_array = []
+# y = 10
+# 20.times do
+#   capacity_array << y
+#   y += 5
+# end
+
+# 8.times do
+#   Event.create!(
+#     # user_id: User.all.sample(:id),
+#     user_id: user_id_array.sample,
+#     venue: Faker::Restaurant.name,
+#     menu: Faker::Lorem.paragraph(sentence_count: 5),
+#     cuisine: Faker::Restaurant.type,
+#     price: price_array.sample,
+#     description: Faker::Restaurant.description,
+#     capacity: capacity_array.sample,
+#     date: Faker::Date.forward(from: ::Date.today, days: 365)
+#   )
+# end
+
+puts "Finished!"
