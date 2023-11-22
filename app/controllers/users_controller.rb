71a87f6ff5c37b2837fba1b1a670a131995daa02
Miriam Ciurea <miriamciurea777@icloud.com>
2023-11-22 08:57:28 +0000

    kind of

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @history = []
-    @upcoming_events = Event.where(user: @user).where('date > ?', Date.today)
-    @events = Event.where(user: @user).where('date <= ?', Date.today)
-    @tickets = Ticket.where(user: @user)
+
+    @upcoming_events = Event.where(user: @user).where('date > ?', Date.today) || []
+    @past_events = Event.where(user: @user).where('date <= ?', Date.today) || []
+
+    # you know what to do
+    @valid_tickets = Ticket.where(user: @user)
+    # .where('date > ?', Date.today) || []
+    # @expired_tickets = Ticket.where(user: @user).where('date <= ?', Date.today) || []
   end
 end
