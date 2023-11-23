84d7739a3e643f01f0c435bc3ebbedb895c8fcbb
Miriam Ciurea <miriamciurea777@icloud.com>
2023-11-23 14:09:14 +0000

    almost there

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   has_many :tickets, dependent: :destroy
   has_many_attached :photos
   validates :user_id, :venue, :menu, :cuisine, :price, :description, :capacity, :date, presence: true
+  geocoded_by :venue
+  after_validation :geocode, if: :will_save_change_to_venue?
 end
